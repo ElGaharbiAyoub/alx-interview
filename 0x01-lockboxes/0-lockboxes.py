20b0fc97bbbdacef38446ae17f4e76bcace4afb0
elgharbi <elgharbiay@gmail.com>
2024-01-12 02:41:06 +0100

    Add lockboxes solution

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    """Determines if all the boxes can be opened."""
+
+    if len(boxes) == 0:
+        return False
+    keys = [0]
+    for key in keys:
+        for box in boxes[key]:
+            if box not in keys and box < len(boxes):
+                keys.append(box)
+    if len(keys) == len(boxes):
+        return True
+    return False
