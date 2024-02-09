3fc098a1a30288846096454a04b78cf22311b6e1
elgharbi <elgharbiay@gmail.com>
2024-02-09 04:05:57 +0100

    from me

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,125 +1,54 @@
 #!/usr/bin/python3
-""" A program that solves the N queens problem
-"""
-from sys import argv
+"""N Queens Challenge"""
+
+import sys
 
 
-def check_row(board, index, board_len):
-    """ Check if there is a queen in the row """
-    for r in range(board_len):
-        if board[index][r]:
-            return (False)
-
-    return (True)
-
-
-def check_r_angle(board, row, col, board_len):
-    """ Check if there is a queen in the left angle """
-    c = col
-    for r in range(row, -1, -1):
-        if c >= board_len:
-            break
-        if board[r][c]:
-            return (False)
-        c += 1
-
-    c = col
-    for r in range(row, board_len):
-        if c < 0:
-            break
-        if board[r][c]:
-            return (False)
-        c -= 1
-
-    return (True)
+def is_safe(row, col, queens):
+    """Check if placing a queen at position (row, col) is safe"""
+    for r, c in queens:
+        if c == col or abs(r - row) == abs(c - col):
+            return False
+    return True
 
 
-def check_l_angle(board, row, col, board_len):
-    """ Check if there is a queen in the right angle """
-    c = col
-    for r in range(row, -1, -1):
-        if c < 0:
-            break
-        if board[r][c]:
-            return (False)
-        c -= 1
+def solve_n_queens(n):
+    """Solve the N Queens problem"""
+    solutions = []
 
-    c = col
-    for r in range(row, board_len):
-        if c >= board_len:
-            break
-        if board[r][c]:
-            return (False)
-        c += 1
+    def backtrack(row, queens):
+        if row == n:
+            solutions.append(queens[:])
+            return
+        for col in range(n):
+            if is_safe(row, col, queens):
+                queens.append([row, col])
+                backtrack(row + 1, queens)
+                queens.pop()
 
-    return (True)
+    backtrack(0, [])
+    return solutions
 
 
-def chek_all(board, r, c, n):
-    if not check_row(board, r, n):
-        return (False)
+def print_solutions(solutions):
+    """Print all solutions"""
+    for queens in solutions:
+        print(queens)
 
-    if not check_l_angle(board, r, c, n):
-        return (False)
-
-    return (check_r_angle(board, r, c, n))
-
-
-def main():
-    """ The Main Function """
-
-    argc = len(argv)
-    if argc != 2:
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
         print("Usage: nqueens N")
-        exit(1)
+        sys.exit(1)
 
     try:
-        n = int(argv[1])
-    except Exception:
-        print("N must be a number")
+        n = int(sys.argv[1])
+    except ValueError:
+        print('N must be a number')
         exit(1)
 
     if n < 4:
-        print("N must be at least 4")
+        print('N must be at least 4')
         exit(1)
 
-    n_range = range(n)
-    i = 0
-    c = 0
-    r = i
-    board = [[0 for _ in n_range] for _ in n_range]
-    result = []
-    while i < n:
-        while (c < n):
-            found = 0
-
-            while (r < n):
-                if chek_all(board, r, c, n):
-                    board[r][c] = 1
-                    result.append([c, r])
-                    found = 1
-                    r = 0
-                    break
-                r += 1
-
-            if not found and len(result):
-                last_i = result.pop()
-                c = last_i[0]
-                r = last_i[1] + 1
-                board[last_i[1]][last_i[0]] = 0
-                continue
-            c += 1
-
-        if len(result):
-            print(result)
-            i = result[0][1]
-            last_i = result.pop()
-            c = last_i[0]
-            r = last_i[1] + 1
-            board[last_i[1]][last_i[0]] = 0
-        else:
-            return
-
-
-if __name__ == "__main__":
-    main()
+    solutions = solve_n_queens(n)
+    print_solutions(solutions)
