651a6d91584732a381f540c7540cd848a519c248
elgharbi <elgharbiay@gmail.com>
2024-02-09 03:49:35 +0100

    Refactor N Queens solution

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,59 +1,51 @@
 #!/usr/bin/python3
+'''N Queens Challenge'''
+
 import sys
 
-def isSafe(board, row, col, n):
-    """Check if a queen can be placed on board[row][col]"""
-    for i in range(col):
-        if board[row][i] == 1:
-            return False
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
-        if board[i][j] == 1:
+def is_safe(row, col, queens):
+    '''Check if placing a queen at position (row, col) is safe'''
+    for r, c in queens:
+        if c == col or abs(r - row) == abs(c - col):
             return False
     return True
 
-def solveNQUtil(board, col, n, solutions):
-    """Use backtracking to solve the N queens problem"""
-    if col == n:
-        add_solution(board, solutions)
-        return True
-    res = False
-    for i in range(n):
-        if isSafe(board, i, col, n):
-            board[i][col] = 1
-            res = solveNQUtil(board, col + 1, n, solutions) or res
-            board[i][col] = 0
-    return res
+def solve_n_queens(n):
+    '''Solve the N Queens problem'''
+    solutions = []
 
-def add_solution(board, solutions):
-    """Add the current board configuration to the solutions list"""
-    n = len(board)
-    solution = []
-    for i in range(n):
-        for j in range(n):
-            if board[i][j] == 1:
-                solution.append([i, j])
-    solutions.append(solution)
+    def backtrack(row, queens):
+        if row == n:
+            solutions.append(queens[:])
+            return
+        for col in range(n):
+            if is_safe(row, col, queens):
+                queens.append([row, col])
+                backtrack(row + 1, queens)
+                queens.pop()
+
+    backtrack(0, [])
+    return solutions
 
 def print_solutions(solutions):
-    """Print all solutions"""
-    for solution in solutions:
-        print(solution)
+    '''Print all solutions'''
+    for sol in solutions:
+        print(sol)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    if not sys.argv[1].isdigit():
-        print("N must be a number")
-        sys.exit(1)
-    n = int(sys.argv[1])
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print('N must be a number')
+        exit(1)
+
     if n < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-    board = [[0 for j in range(n)] for i in range(n)]
-    solutions = []
-    solveNQUtil(board, 0, n, solutions)
+        print('N must be at least 4')
+        exit(1)
+
+    solutions = solve_n_queens(n)
     print_solutions(solutions)
