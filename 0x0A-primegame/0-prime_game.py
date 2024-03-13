0bb6e5aee065cfd677398d2247505d216188b8ac
elgharbi <elgharbiay@gmail.com>
2024-03-13 14:03:00 +0000

    Add prime game implementation and test script

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+Prime Game
+"""
+
+
+
+def isPrime(n):
+    """
+    Check if a number is prime
+    """
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def isWinner(x, nums):
+    """
+    Prime Game
+    """
+    def countPrimes(n):
+        """
+        Count the number of prime numbers up to n
+        """
+        count = 0
+        for i in range(1, n + 1):
+            if isPrime(i):
+                count += 1
+        return count
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for num in nums:
+        primes = countPrimes(num)
+        if primes % 2 == 0:
+            ben_wins += 1
+        else:
+            maria_wins += 1
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif maria_wins < ben_wins:
+        return "Ben"
+    else:
+        return None
