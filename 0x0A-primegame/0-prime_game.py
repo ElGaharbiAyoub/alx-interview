af251c24144be32a438429327ff8c34a2aa1ae94
elgharbi <elgharbiay@gmail.com>
2024-03-13 14:31:27 +0000

    Refactor prime game logic

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,37 +3,41 @@
 Prime Game
 """
 
+def isPrime(n):
+    """
+    Check if a number is prime
+    """
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def countPrimes(n):
+    """
+    Count the number of primes
+    """
+    count = 0
+    for i in range(1, n + 1):
+        if isPrime(i):
+            count += 1
+    return count
+
 
 def isWinner(x, nums):
     """
     Prime Game
     """
-    def isPrime(n):
-        """
-        Check if a number is prime
-        """
-        if n <= 1:
-            return False
-        for i in range(2, int(n**0.5) + 1):
-            if n % i == 0:
-                return False
-        return True
-
-    def countPrimes(n):
-        """
-        Count the number of primes
-        """
-        count = 0
-        for i in range(1, n + 1):
-            if isPrime(i):
-                count += 1
-        return count
+    if x is None or nums is None or x == 0 or nums == []:
+        return None
 
     maria_wins = 0
     ben_wins = 0
 
-    for num in nums:
-        primes = countPrimes(num)
+    for i in range(x):
+        primes = countPrimes(nums[i])
         if primes % 2 == 0:
             ben_wins += 1
         else:
