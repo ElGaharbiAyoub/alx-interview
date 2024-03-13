98647e46a8e4ce52522186f02ced2824935bf625
elgharbi <elgharbiay@gmail.com>
2024-03-13 14:12:12 +0000

    Refactor isPrime and countPrimes functions

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,26 +4,16 @@ Prime Game
 """
 
 
-def isPrime(n):
-    """
-    Check if a number is prime
-    """
-    if n <= 1:
-        return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
-
-
 def isWinner(x, nums):
-    """
-    Prime Game
-    """
+    def isPrime(n):
+        if n <= 1:
+            return False
+        for i in range(2, int(n**0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
+
     def countPrimes(n):
-        """
-        Count the number of prime numbers up to n
-        """
         count = 0
         for i in range(1, n + 1):
             if isPrime(i):
