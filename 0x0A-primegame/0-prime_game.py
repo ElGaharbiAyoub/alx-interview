41e63e2b9184e284ee9037238049e128c66f8f5e
elgharbi <elgharbiay@gmail.com>
2024-03-13 14:14:17 +0000

    Add prime number utility functions

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -5,7 +5,13 @@ Prime Game
 
 
 def isWinner(x, nums):
+    """
+    Prime Game
+    """
     def isPrime(n):
+        """
+        Check if a number is prime
+        """
         if n <= 1:
             return False
         for i in range(2, int(n**0.5) + 1):
@@ -14,6 +20,9 @@ def isWinner(x, nums):
         return True
 
     def countPrimes(n):
+        """
+        Count the number of primes
+        """
         count = 0
         for i in range(1, n + 1):
             if isPrime(i):
