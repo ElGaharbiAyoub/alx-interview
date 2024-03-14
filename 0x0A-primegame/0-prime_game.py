a1d03d7a9bbeb250048d7103822b8b79b781f2e7
elgharbi <elgharbiay@gmail.com>
2024-03-14 23:12:27 +0000

    Refactor prime counting logic in isWinner function

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -16,6 +16,17 @@ def isPrime(n):
     return True
 
 
+def countPrimes(n):
+    """
+    Count the number of primes
+    """
+    count = 0
+    for i in range(1, n + 1):
+        if isPrime(i):
+            count += 1
+    return count
+
+
 def isWinner(x, nums):
     """
     Prime Game
@@ -27,11 +38,11 @@ def isWinner(x, nums):
     ben_wins = 0
 
     for i in range(x):
-        if isPrime(nums[i]):
-            if nums[i] % 2 == 0:
-                ben_wins += 1
-            else:
-                maria_wins += 1
+        primes = countPrimes(nums[i])
+        if primes % 2 == 0:
+            ben_wins += 1
+        else:
+            maria_wins += 1
 
     if maria_wins > ben_wins:
         return "Maria"
