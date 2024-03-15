abd8a93d2afd45dac8e295c47064346849537f74
elgharbi <elgharbiay@gmail.com>
2024-03-15 01:18:42 +0000

    Refactor prime game logic to improve performance

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,44 +3,33 @@
 Prime Game
 """
 
-
-def sieveOfEratosthenes(n):
-    """
-    Generate all primes up to n
-    """
-    primes = [True for i in range(n+1)]
-    p = 2
-    while (p * p <= n):
-        if (primes[p]):
-            for i in range(p * p, n+1, p):
-                primes[i] = False
-        p += 1
-    primes[0] = False
-    primes[1] = False
-    return primes
+def getPrimes(n):
+    """ list of primes"""
+    prime = []
+    sieve = [True] * (n + 1)
+    for p in range(2, n + 1):
+        if (sieve[p]):
+            prime.append(p)
+            for i in range(p, n + 1, p):
+                sieve[i] = False
+    return prime
 
 
 def isWinner(x, nums):
     """
-    Prime Game
+        define winner
     """
     if x is None or nums is None or x == 0 or nums == []:
         return None
-
-    maria_wins = 0
-    ben_wins = 0
-
-    primes = sieveOfEratosthenes(max(nums))
+    maria_count = ben_count = 0
     for i in range(x):
-        prime = sieveOfEratosthenes(nums[i])
-        if sum(prime) % 2 == 0:
-            ben_wins += 1
+        prime = getPrimes(nums[i])
+        if len(prime) % 2 == 0:
+            ben_count += 1
         else:
-            maria_wins += 1
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif maria_wins < ben_wins:
-        return "Ben"
-    else:
-        return None
+            maria_count += 1
+    if maria_count > ben_count:
+        return 'Maria'
+    elif ben_count > maria_count:
+        return 'Ben'
+    return None
